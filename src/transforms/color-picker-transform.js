ebafcd17cc4d3329602a7bf33c7b15ce1dbf7809
Алексей Никитченко <achestron@gmail.com>
2022-10-08 13:39:00 +0300

    Добавляет исключение

--- src/transforms/color-picker-transform.js
+++ src/transforms/color-picker-transform.js
@@ -5,5 +5,13 @@ module.exports = function (window) {
   const content = window.document.querySelector('.article__content-inner')
   const color = content?.querySelectorAll('.token.color')
 
-  color?.forEach((item) => item.style.setProperty('--color-picker', ` ${item.textContent}`))
+  color?.forEach(function (item) {
+    item.style.setProperty('--color-picker', ` ${item.textContent}`)
+
+    if (/[(]/.test(item.previousElementSibling.textContent)) {
+      item.classList.add('color-picker__internal') // Добавляет дополнительный margin слева, если токен цвета внутри скобок
+    } else {
+      item.classList.add('color-picker__external')
+    }
+  })
 }
